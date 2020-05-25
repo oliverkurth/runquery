cb9e128da08807584e9491bad559c960fc110a6d
Oliver Kurth <okurth@gmail.com>
2020-05-25 11:04:16 -0700

    client.exchange_code_for_token returns a dict, not the token itself

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import json
 
 app = Flask(__name__)
 app.secret_key = "piesakeGhieh3xeipheifaem7eijai9e"
-client = Client()
 access_token = None
 
 CLIENT_ID='3724'
@@ -16,7 +15,8 @@ CLIENT_SECRET='STRAVA_CLIENT_SECRET'
 
 @app.route('/athlete')
 def show_athlete():
-    access_token = session['access_token']
+    token_struct = session['token_struct']
+    client = Client(access_token=token_struct['access_token'])
     athlete = client.get_athlete()
     return json.dumps(athlete.to_dict())
 #    return "Name: {}, email: {}".format(athlete.firstname, athlete.email)
@@ -24,7 +24,8 @@ def show_athlete():
 @app.route('/activity')
 def show_activity():
     id = request.args.get('id')
-    access_token = session['access_token']
+    token_struct = session['token_struct']
+    client = Client(access_token=token_struct['access_token'])
     activity = client.get_activity(id)
     return "start: {}, end: {}".format(activity.start_latlng, activity.end_latlng)
     #return json.dumps(activity.to_dict())
@@ -32,19 +33,21 @@ def show_activity():
 @app.route('/authorized')
 def authorized():
     code = request.args.get('code')
-    access_token = client.exchange_code_for_token(
+    client = Client()
+    token_struct = client.exchange_code_for_token(
         client_id=CLIENT_ID,
         client_secret=CLIENT_SECRET,
         code=code
     )
     athlete = client.get_athlete()
-    session['access_token'] = access_token
+    session['token_struct'] = token_struct
     return ("For {id}, I now have an access token {token}".format(
-        id=athlete.id, token=access_token))
+        id=athlete.id, token=token_struct))
 
 @app.route('/')
 def index():
     authorize_url = None
+    client = Client()
     if not access_token:
         authorize_url = client.authorization_url(
             client_id=CLIENT_ID,
@@ -52,7 +55,6 @@ def index():
         return redirect(authorize_url, code=302)
 
     print access_token
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8282)
